2ca42db1f732d41fc65d2d46844f25a6d475c00c
Ankur Seth <iankurseth@gmail.com>
2023-08-29 19:31:50 +0530

    Updated json structure

--- process.py
+++ process.py
@@ -5,13 +5,13 @@ import json
 txt_path = "kauravas.txt"
 text = open(txt_path).read()
 lines = [line.strip() for line in text.split("\n") if line]
-names = [line.split()[1].strip() for line in lines]
-print(f"Found {len(names)} names in {txt_path}.")
+kauravas_names = [line.split()[1].strip() for line in lines]
+print(f"Found {len(kauravas_names)} names in {txt_path}.")
 
 jo = {}
-jo["names"] = names
-json_path = "kauravas.json"
+jo["kauravas"] = kauravas_names
+json_path = "out/kauravas.json"
 with open(json_path, "w") as f:
-	json.dump(names, f)
+	json.dump(jo, f)
 
 print(f"Saved: {json_path}")
